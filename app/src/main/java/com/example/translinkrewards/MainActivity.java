466e51e4b38a7e86794e9ca8fa4b9ea7d2520b1a
keyiyu <yiyu.ke@dreampix.cn>
2020-08-23 23:25:55 +0800

    function: Jump Page main -> login/register; register -> login

--- app/src/main/java/com/example/translinkrewards/MainActivity.java
+++ app/src/main/java/com/example/translinkrewards/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.translinkrewards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * The first page of this app, contain two buttons and one imageView
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * use login button go to LoginPage, start login action
+     * @param view
+     */
+    public void goToLoginPage(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * use register button go to registerPage, start register action
+     * @param view
+     */
+    public void goToRegisterPage(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/translinkrewards/RegisterActivity.java
+++ app/src/main/java/com/example/translinkrewards/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.translinkrewards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void beginLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
